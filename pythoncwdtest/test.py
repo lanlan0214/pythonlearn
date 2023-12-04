165615ac419fddfce99470b9b3ad83377d3a8af2
Frank <franklp5010606@gmail.com>
2023-12-04 17:32:28 +0800

    202312041732-抓ptt圖

---
+++ pythoncwdtest/test.py
@@ -0,0 +1,5 @@
+import os
+
+print(os.pardir)
+print(os.listdir(os.pardir))
+# 這樣會因為不同作業系統 而改變
---
+++ pythoncwdtest/tttt.py
@@ -0,0 +1,27 @@
+
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# 取得使用者輸入的目錄名稱和網址
+folder_name = input("請輸入目錄名稱： ")
+url = input("請輸入網址： ")
+
+# 創建目錄
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+web = requests.get(url, cookies={'over18': '1'})
+soup = BeautifulSoup(web.text, "html.parser")
+imgs = soup.find_all('img')
+name = 0    #  設定圖片編號
+
+for i in imgs:
+    print(i['src'])
+    jpg = requests.get(i['src'])
+    # 使用 os.path.join 來建立路徑
+    file_path = os.path.join(folder_name, f'test_{name}.jpg')
+    with open(file_path, 'wb') as f:
+        f.write(jpg.content)
+    name += 1
