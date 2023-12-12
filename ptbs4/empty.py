5e64dd358fbbe86751c749c0a01181c43cabf908
Frank <franklp5010606@gmail.com>
2023-12-12 16:04:27 +0800

    202312121604-yahoo股價

--- ptbs4/empty.py
+++ ptbs4/empty.py
@@ -1,1 +1,33 @@
 #pip install bs4
+#pip install lxml
+from bs4 import BeautifulSoup
+
+# 如果要貼比較多string 就要用三個"""符號
+text = """
+    <html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p class="title hello item">123</p>
+    <a target="_blank" href="https://www.google.com" >123</a>
+    <p class="test">321</p>
+</body>
+</html>
+"""
+
+soup = BeautifulSoup(text, 'lxml')
+# print(soup.find("p"))
+# # 這邊尋找如果要找class要加個底線
+# print(soup.find(class_="title"))
+# # 想找p tag然後class是title
+# print(soup.find("p" , class_="title"))
+# # 找全部
+# print(len(soup.find_all("p" )))
+# tag的
+first_p_tag = soup.find("p")
+print(first_p_tag.get("class"))
+# 這是看這裡面內容
+print(first_p_tag.getText())
---
+++ ticket2330/empty.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+import concurrent.futures
+from datetime import datetime
+import time
+
+# 初始化前一次的股票資訊字典
+previous_stock_info = {}
+
+# 定義股票列表
+stock_urls = {
+    '2330': 'https://tw.stock.yahoo.com/quote/2330',
+    '6104': 'https://tw.stock.yahoo.com/quote/6104',
+    '8299': 'https://tw.stock.yahoo.com/quote/8299',
+    '3443': 'https://tw.stock.yahoo.com/quote/3443',
+    '2377': 'https://tw.stock.yahoo.com/quote/2377',
+    # 添加更多股票的 URL
+}
+
+def get_stock_info(stock_code, url):
+    global previous_stock_info
+
+    web = requests.get(url)
+    soup = BeautifulSoup(web.text, "html.parser")
+    title = soup.find('h1', class_="C($c-link-text) Fw(b) Fz(24px) Mend(8px)")
+    a = soup.select('.Fz\(32px\)')[0]
+    b = soup.select('.Fz\(20px\)')[0]
+    s = ''
+    try:
+        if soup.select('#main-0-QuoteHeader-Proxy')[0].find('span', class_="C($c-trend-down)"):
+            s = '-'
+    except:
+        try:
+            if soup.select('#main-0-QuoteHeader-Proxy')[0].find('span', class_='C($c-trend-up)'):
+                s = '+'
+        except:
+            s = ''
+
+    # 使用 strip() 函數去掉 b 中的空白字符
+    b_value = b.get_text().strip()
+
+    current_stock_info = f'{title.get_text()} : {a.get_text()} ( {s}{b_value} )'
+
+    # 如果 b 的值為 0，則跳過自動刷新的步驟
+    if b_value == '0':
+        print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {current_stock_info} (Skipping refresh, b is 0)')
+    else:
+        # 比較前一次和當前的股票資訊，如果有變動才顯示
+        if current_stock_info != previous_stock_info.get(stock_code):
+            print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {current_stock_info}')
+            previous_stock_info[stock_code] = current_stock_info
+
+# 設定自動刷新的間隔時間（秒）
+refresh_interval = 10
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    while True:
+        # 使用 ThreadPoolExecutor 來同時爬取多支股票的資訊
+        futures = {executor.submit(get_stock_info, stock_code, stock_url): stock_code for stock_code, stock_url in stock_urls.items()}
+        concurrent.futures.wait(futures)
+
+        time.sleep(refresh_interval)
