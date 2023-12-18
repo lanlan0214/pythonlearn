74a9f111c02003e5e3db1eecd717990674bb96ad
unknown <franklp5010606@gmail.com>
2023-12-18 22:20:04 +0800

    202312182220

--- Chapter14/inDatabases/empty.py
+++ Chapter14/inDatabases/empty.py
@@ -1,6 +1,13 @@
-# SQLite
+# SQLite 存進去資料到資料庫裡面
 import sqlite3
 
 conn = sqlite3.connect("datafile.db")
 cursor = conn.cursor()
-cursor.execute("""""")
+cursor.execute("""create table people (id integer primary key, name text, count integer)""")
+cursor.execute("""insert into people (name, count) values (?, ?)""", ('Bob', 25))
+cursor.execute("""insert into people (name, count) values (:username, :usercount)""", {"username" : "Frank" , "usercount" : 27})
+
+conn.commit()
+conn.close()
+
+# 讀取資料庫裏面的資訊
