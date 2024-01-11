1f62e5bfc20d5c0bdfa5af3856cee4efa16cc94d
unknown <franklp5010606@gmail.com>
2024-01-11 11:33:20 +0800

    202401111133-現金資料出現

--- 個人理財網站/index.py
+++ 個人理財網站/index.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, g, redirect
+import requests
 import sqlite3
+import math
 
 app = Flask(__name__)
 
@@ -18,7 +20,25 @@ def close_connection(exception):
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  conn = get_db()
+  cursor = conn.cursor()
+  result = cursor.execute('select * from cash')
+  cash_result = result.fetchall()
+  # 計算台幣與美金的總額
+  taiwanese_dollars = 0
+  us_dollars = 0
+  for data in cash_result:
+    taiwanese_dollars += data[1]
+    us_dollars += data[2]
+  # 獲取匯率資訊
+  r=requests.get('https://tw.rter.info/capi.php')
+  currency=r.json()
+  total = math.floor(taiwanese_dollars + us_dollars * currency['USDTWD']['Exrate'])
+
+  data = {'total': total, 'currency' : currency['USDTWD']['Exrate'], 'ud': us_dollars, 'td': taiwanese_dollars, 'cash_result': cash_result}
+  
+  return render_template('index.html', data=data)
+  # 上面這個data=data是讓index這個可以使用資料
 
 @app.route('/cash')
 def cash_form():
